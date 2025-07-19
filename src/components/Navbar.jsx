5f30e661e4d9e05690c497561d9ac0f6ded8d1cd
cesar91bo <cesar91bo@gmail.com>
2025-07-19 14:42:05 -0300

    agregué redes sociales

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,18 @@ const Navbar = () => {
         <img className="mx-2 w-30" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-between gap-6 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaWhatsapp />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/cesar-ramirez-8047b974/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="hover:text-cyan-400 transition-colors duration-200" />
+        </a>
+        <a href="https://github.com/cesar91bo" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="hover:text-cyan-400 transition-colors duration-200" />
+        </a>
+        <a href="https://wa.me/5493725403098" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp className="hover:text-cyan-400 transition-colors duration-200" />
+        </a>
+        <a href="https://www.instagram.com/cesar91bo/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="hover:text-cyan-400 transition-colors duration-200" />
+        </a>
       </div>
     </nav>
   )
