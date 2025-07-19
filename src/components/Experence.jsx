c7f66c52a87b750302ad1f952daa2b7ba5bb99e2
cesar91bo <cesar91bo@gmail.com>
2025-07-19 14:07:29 -0300

    se agregó frame motion para darle movimientos a los componentes

--- src/components/Experence.jsx
+++ src/components/Experence.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { EXPERIENCES } from '../constants'
+import {motion} from 'framer-motion'
 
 const Experence = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h2 className='my-20 text-center text-4xl'>Experiencia</h2>
+        <motion.h2 
+            whileInView={{opacity: 1, y:0}}
+            initial={{opacity:0, y: -100}}
+            transition={{duration: 0.5}}
+            className='my-20 text-center text-4xl'>Experiencia
+        </motion.h2>
         <div>
             {EXPERIENCES.map((experience, index) => (
                 <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                    <div className='w-full lg:w-1/4'>
+                    <motion.div
+                        whileInView={{opacity: 1, x:0}}
+                        initial={{opacity:0, x: -100}}
+                        transition={{duration: 1}}
+                        className='w-full lg:w-1/4'>
                         <p className='mb-2 text-sm text-neutral-400'>{experience.year}</p>
-                    </div>
-                    <div className='w-full max-w-xl'>
+                    </motion.div>
+                    <motion.div 
+                        whileInView={{opacity: 1, x:0}}
+                        initial={{opacity:0, x: 100}}
+                        transition={{duration: 1}}
+                        className='w-full max-w-xl'>
                         <h6 className='mb-2 font-semibold'>
                             {experience.role} - {" "}
                             <span className='text-sm text-purple-100'>
@@ -25,7 +39,7 @@ const Experence = () => {
                                 className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium
                                 text-purple-800'>{tech}</span>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             ))}
         </div>
